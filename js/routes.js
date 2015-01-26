3edff318fbc27ca9004f2b7ca4ea253b16159953
Pedro Santos <donbonifacio@gmail.com>
2015-01-26 11:50:04 +0000

    Serve app via http-server

--- js/routes.js
+++ js/routes.js
@@ -8,9 +8,7 @@ var TodoApp = require('./components/TodoApp.react.js');
 var AboutPage = require('./components/AboutPage.react.js');
 
 module.exports = (
-  <Route name='todo' path='/index.html' handler={TodoApp}>
-    <Route name='todofileaccess' path='/Users/pedrosantos/projects/donbonifacio/flux-playground/index.html' handler={TodoApp}>
-      <Route name="about" path="about" handler={AboutPage} />
-    </Route>
+  <Route name='todo' path='/' handler={TodoApp}>
+    <Route name="about" path="about" handler={AboutPage} />
   </Route>
 );
